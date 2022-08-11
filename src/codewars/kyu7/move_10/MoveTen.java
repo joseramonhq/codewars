3fac21caa4cf125196954c8260de0c245bbadf4f
Jose Ramon Hidalgo Quintana <joseramonhq@gmail.com>
2022-08-11 23:38:53 +0200

    Another version with array of letters

--- src/codewars/kyu7/move_10/MoveTen.java
+++ src/codewars/kyu7/move_10/MoveTen.java
@@ -1,11 +1,13 @@
 package codewars.kyu7.move_10;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MoveTen {
     public static String moveTen(String s) {
-        return s.chars()
-                .map(x -> (((x - 97) + 10) % 26) + 97)
-                .collect(StringBuilder::new,
-                        StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
+        String letters = "abcdefghijklmnopqrstuvwxyz";
+        return Stream.of(s.split(""))
+                .map(x -> letters.charAt((letters.indexOf(x) + 10) % 26) + "")
+                .collect(Collectors.joining());
     }
 }
