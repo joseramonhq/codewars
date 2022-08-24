2800118bb7534c62d7bd8df281d29b954ebe0c13
Jose Ramon Hidalgo Quintana <joseramonhq@gmail.com>
2022-08-24 18:43:52 +0200

    with Streams

--- src/codewars/kyu7/find_the_nth_occurrence_of_a_word_in_a_string/FindOcurrence.java
+++ src/codewars/kyu7/find_the_nth_occurrence_of_a_word_in_a_string/FindOcurrence.java
@@ -1,15 +1,15 @@
 package codewars.kyu7.find_the_nth_occurrence_of_a_word_in_a_string;
 
+import java.util.stream.IntStream;
+
 public class FindOcurrence {
+
     public static int findNthOccurrence(String subStr, String str, int occurrence) {
-        int counter = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.substring(i).startsWith(subStr))
-                counter++;
-            if (counter == occurrence)
-                return i;
-        }
-        return -1;
+
+        int[] a = IntStream.range(0, str.length())
+                .filter(i -> str.substring(i).startsWith(subStr))
+                .toArray();
+        return a.length < occurrence ? -1 : a[occurrence - 1];
     }
 }
 
