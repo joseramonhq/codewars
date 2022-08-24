dc226ecb1db25fef7001171866df9ae11b74f7e7
Jose Ramon Hidalgo Quintana <joseramonhq@gmail.com>
2022-08-24 18:38:25 +0200

    updated

--- src/codewars/kyu7/find_the_nth_occurrence_of_a_word_in_a_string/FindOcurrence.java
+++ src/codewars/kyu7/find_the_nth_occurrence_of_a_word_in_a_string/FindOcurrence.java
@@ -2,9 +2,14 @@ package codewars.kyu7.find_the_nth_occurrence_of_a_word_in_a_string;
 
 public class FindOcurrence {
     public static int findNthOccurrence(String subStr, String str, int occurrence) {
-
-        return 0;
-
+        int counter = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.substring(i).startsWith(subStr))
+                counter++;
+            if (counter == occurrence)
+                return i;
+        }
+        return -1;
     }
 }
 
