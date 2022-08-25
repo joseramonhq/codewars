649ae35edea4894e9aa10c2a3d96d58aa4c06d64
Jose Ramon Hidalgo Quintana <joseramonhq@gmail.com>
2022-08-25 21:51:34 +0200

    delete comments

--- src/codewars/kyu7/simple_array_product/Solution.java
+++ src/codewars/kyu7/simple_array_product/Solution.java
@@ -34,24 +34,4 @@ class Solution {
             }
         }
     }
-
-    /*public static int solve(int[][] arr) {
-        String[] combinations = IntStream.rangeClosed(0, (int) Math.pow(2, arr.length))
-                .mapToObj(x -> String.format("%0" + arr.length + "d", Integer.parseInt(Integer.toBinaryString(x))))
-                .toArray(String[]::new);
-        System.out.println(Arrays.toString(combinations));
-        int max = 1;
-        int prod = 1;
-        List<Integer> results = new ArrayList<>();
-        for (int i = 0; i < combinations.length; i++) {
-            prod = 1;
-            for (int j = 0; j < arr.length; j++) {
-                System.out.println();
-                prod *= Integer.parseInt(String.valueOf(arr[j][Integer.parseInt(combinations[i].charAt(j) + "")]));
-            }
-            results.add(prod);
-        }
-        return Collections.max(results);
-
-    }*/
 }
