51e38a80d63cc1a82107aa96a184b3f4a67289fb
Jose Ramon Hidalgo Quintana <joseramonhq@gmail.com>
2022-08-26 01:10:48 +0200

    updated to one line

--- src/codewars/kyu7/simple_array_product/Solution.java
+++ src/codewars/kyu7/simple_array_product/Solution.java
@@ -1,37 +1,24 @@
 package codewars.kyu7.simple_array_product;
 
+import com.google.common.collect.Sets;
+
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 class Solution {
 
     public static int solve(int[][] arr) {
-        int max = Integer.MIN_VALUE;
-        int index = arr.length - 1;
-        int[] mutantIndex = new int[arr.length];
-        int combinations = Stream.of(arr)
-                .mapToInt(x -> x.length)
-                .reduce(1, (a, b) -> a * b);
-
-        for (int i = 0; i < combinations; i++) {
-            int prod = IntStream.range(0, arr.length)
-                    .map(x -> arr[x][mutantIndex[x]])
-                    .reduce(1, (a, b) -> a * b);
-            if (prod > max) max = prod;
-            handler(mutantIndex, index, arr);
-        }
-        return max;
-
+        return Sets.cartesianProduct(Stream.of(arr)
+                .map(t -> IntStream.of(t)
+                        .boxed()
+                        .collect(Collectors.toSet()))
+                .toList())
+                .stream()
+                .map(x -> x.stream()
+                        .reduce(1, (c, b) -> c * b))
+                .max(Integer::compare)
+                .orElse(0);
     }
 
-    public static void handler(int[] counter, int index, int[][] arr) {
-        counter[index]++;
-        if (counter[index] >= arr[index].length) {
-            counter[index] = 0;
-            index--;
-            if (index >= 0) {
-                handler(counter, index, arr);
-            }
-        }
-    }
 }
