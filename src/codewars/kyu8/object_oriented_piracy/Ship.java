6accaf902bb69830eb9397ae67054dd299d2b656
Jose Ramon Hidalgo Quintana <joseramonhq@gmail.com>
2022-10-26 18:31:21 +0200

    update to Java 17

--- src/codewars/kyu8/object_oriented_piracy/Ship.java
+++ src/codewars/kyu8/object_oriented_piracy/Ship.java
@@ -1,16 +1,9 @@
 package codewars.kyu8.object_oriented_piracy;
 
-public class Ship {
-    private final double draft;
-    private final int crew;
-
-    public Ship(double draft, int crew) {
-        this.draft = draft;
-        this.crew = crew;
-    }
+record Ship(double draft, int crew) {
 
     public boolean isWorthIt() {
-        return draft - crew * 1.5 > 20;
+        return draft - (crew * 1.5) > 20;
     }
 }
 
